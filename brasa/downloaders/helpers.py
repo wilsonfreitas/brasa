5315b0966938f66eaaac37b689332fe129218ccd
wilsonfreitas <wilson.freitas@gmail.com>
2023-06-04 08:31:19 -0300

    Typing errors corrected

--- brasa/downloaders/helpers.py
+++ brasa/downloaders/helpers.py
@@ -9,16 +9,16 @@ def simple_download(md_downloader: MarketDataDownloader, **kwargs) -> tuple[IO |
     return downloader.download(), dict(downloader.response.headers)
 
 
-def datetime_download(md_downloader: MarketDataDownloader, **kwargs) -> IO | None:
+def datetime_download(md_downloader: MarketDataDownloader, **kwargs) -> tuple[IO | None, dict[str, str]]:
     downloader = DatetimeDownloader(md_downloader.url, md_downloader.verify_ssl, **kwargs)
     return downloader.download(), dict(downloader.response.headers)
 
 
-def b3_url_encoded_download(md_downloader: MarketDataDownloader, **kwargs) -> IO | None:
+def b3_url_encoded_download(md_downloader: MarketDataDownloader, **kwargs) -> tuple[IO | None, dict[str, str]]:
     downloader = B3URLEncodedDownloader(md_downloader.url, md_downloader.verify_ssl, **kwargs)
     return downloader.download(), dict(downloader.response.headers)
 
 
-def settlement_prices_download(md_downloader: MarketDataDownloader, **kwargs) -> IO | None:
+def settlement_prices_download(md_downloader: MarketDataDownloader, **kwargs) -> tuple[IO | None, dict[str, str]]:
     downloader = SettlementPricesDownloader(md_downloader.url, md_downloader.verify_ssl, **kwargs)
     return downloader.download(), dict(downloader.response.headers)
