fee7eef15e856d3d135782ca080672364b92944a
wilsonfreitas <wilson.freitas@gmail.com>
2023-06-25 06:00:38 -0300

    date -d argument accepts multiple values

--- brasa/cli.py
+++ brasa/cli.py
@@ -1,5 +1,6 @@
 
 import argparse
+from datetime import datetime
 import brasa
 
 from brasa.util import DateRangeParser
@@ -8,7 +9,7 @@ parser = argparse.ArgumentParser()
 
 subparsers = parser.add_subparsers(dest="command", title="Commands")
 parser_download = subparsers.add_parser("download", help="download market data")
-parser_download.add_argument("-d", "--date", "--date-range", help="specify date or date range to download and process market data")
+parser_download.add_argument("-d", "--date", "--date-range", nargs="+", help="specify date or date range to download and process market data")
 parser_download.add_argument("--calendar", help="specify calendar to be used for creating date range", default="B3",
                              choices=["B3", "ANBIMA", "actual"])
 parser_download.add_argument("template", nargs="+", help="template names")
@@ -23,7 +24,10 @@ parser_show.add_argument("choice", choices=["templates"])
 if __name__ == "__main__":
     args = parser.parse_args()
     if args.command == "download":
-        date_range = DateRangeParser(args.calendar).parse(args.date)
+        if len(args.date) == 1:
+            date_range = DateRangeParser(args.calendar).parse(args.date[0])
+        else:
+            date_range = [datetime.strptime(d, "%Y-%m-%d") for d in args.date]
         for template in args.template:
             brasa.download_marketdata(template, refdate=date_range)
     elif args.command == "process":
