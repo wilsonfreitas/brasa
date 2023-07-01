cc846b15844e065ced48460e9c02a83812e57680
wilsonfreitas <wilson.freitas@gmail.com>
2023-07-01 09:05:12 -0300

    BUG corrected bad formated files

--- brasa/readers/helpers.py
+++ brasa/readers/helpers.py
@@ -212,7 +212,7 @@ def read_b3_bvbg086(meta: CacheMetadata) -> pd.DataFrame:
     df["previous_adjusted_quote"] = pd.to_numeric(df["previous_adjusted_quote"])
     df["previous_adjusted_tax"] = pd.to_numeric(df["previous_adjusted_tax"])
     df["variation_points"] = pd.to_numeric(df["variation_points"])
-    df["adjusted_value_contract"] = pd.to_numeric(df["adjusted_value_contract"])
+    df["adjusted_value_contract"] = pd.to_numeric(df["adjusted_value_contract"].str.replace(",", "."))
     df["nonregular_transactions_quantity"] = pd.to_numeric(df["nonregular_transactions_quantity"])
     df["nonregular_traded_contracts"] = pd.to_numeric(df["nonregular_traded_contracts"])
     df["nonregular_volume"] = pd.to_numeric(df["nonregular_volume"])
