c6564cb0e2b754c0fdae7956a265d756a96de873
wilsonfreitas <wilson.freitas@gmail.com>
2023-07-01 07:00:41 -0300

    adding functions to brasa.* namespace

--- brasa/__init__.py
+++ brasa/__init__.py
@@ -2,4 +2,5 @@
 from datetime import datetime
 
 import pandas as pd
-from brasa.engine import get_marketdata, download_marketdata, process_marketdata
+from brasa.engine import get_marketdata, download_marketdata, process_marketdata, CacheManager
+from brasa.queries import *
