42510f9ec94cb0b68b604ecec1fc69df164dc4c4
wilsonfreitas <wilson.freitas@gmail.com>
2023-12-20 07:45:17 -0300

    use relative imports

--- brasa/__init__.py
+++ brasa/__init__.py
@@ -2,11 +2,11 @@
 from datetime import datetime
 
 import pandas as pd
-from brasa.engine import (
+from .engine import (
     get_marketdata,
     download_marketdata,
     process_marketdata,
     CacheManager,
     process_etl,
     retrieve_template)
-from brasa.queries import *
+from .queries import *
