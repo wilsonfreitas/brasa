265f3494257bb745c622f69cc73639f25ec63607
Wilson Freitas <wilson.freitas@gmail.com>
2025-11-29 22:11:04 -0300

    Update market data download calls to include additional symbols and refactor existing calls

--- cli-random.py
+++ cli-random.py
@@ -2,14 +2,74 @@
 # os.environ["BRASA_DATA_PATH"] = "D:\\brasa"
 
 from datetime import datetime
+
 import brasa
 from brasa.util import DateRange
 
-# brasa.download_marketdata("b3-cash-dividends", tradingName="BRADESCO")
-brasa.download_marketdata("b3-cash-dividends", tradingName=brasa.get_symbols("company-trading-name"))
+period = DateRange(start=datetime(2025, 1, 1), end=datetime.today(), calendar="B3")
+
+brasa.download_marketdata("b3-bvbg087", refdate=period)
+
+brasa.download_marketdata("b3-bvbg028", refdate=period)
+
+brasa.download_marketdata("b3-bvbg086", refdate=period)
+
+brasa.download_marketdata("b3-futures-settlement-prices", refdate=period)
+
+brasa.download_marketdata("b3-cotahist-daily", refdate=period)
+
+# brasa.download_marketdata("b3-lending-trades", refdate=period)
+
+brasa.download_marketdata("b3-otc-trade-information", refdate=period)
+
+# brasa.download_marketdata("b3-economic-indicators-price", refdate=period)
+
+brasa.download_marketdata("b3-economic-indicators-fwf", refdate=period)
+
+# brasa.download_marketdata("b3-equities-volatility-surface", refdate=period)
+
+# brasa.download_marketdata("b3-companies-options", refdate=period)
+
+# brasa.download_marketdata("b3-indexes-composition")
+
+# brasa.download_marketdata("b3-indexes-theoretical-portfolio", index=brasa.get_symbols("index"))
+
+# brasa.download_marketdata("b3-company-info", issuingCompany=brasa.get_symbols("company"))
+# brasa.process_marketdata("b3-company-info")
+# brasa.process_etl("b3-companies-info")
+# # depends on b3-companies-info
+# brasa.download_marketdata("b3-company-details", codeCVM=brasa.get_symbols("company-cvm-code"))
+# brasa.process_marketdata("b3-company-details")
+# brasa.process_etl("b3-companies-details")
+# # depends on b3-companies-details
+# brasa.download_marketdata("b3-cash-dividends", tradingName=brasa.get_symbols("company-trading-name"))
 # brasa.process_marketdata("b3-cash-dividends")
 # brasa.process_etl("b3-companies-properties")
 # brasa.process_etl("b3-equity-symbols-properties")
 # brasa.process_etl("b3-companies-cash-dividends")
 # brasa.process_etl("b3-companies-stock-dividends")
 # brasa.process_etl("b3-companies-subscriptions")
+
+brasa.download_marketdata("b3-listed-fixed-income-etfs")
+brasa.download_marketdata("b3-listed-stock-etfs")
+brasa.download_marketdata("b3-listed-reits")
+
+brasa.download_marketdata("b3-trades-intraday", refdate=period)
+
+
+# brasa.process_marketdata("b3-bvbg087")
+# brasa.process_marketdata("b3-bvbg028")
+# brasa.process_marketdata("b3-bvbg086")
+# brasa.process_marketdata("b3-futures-settlement-prices")
+# brasa.process_marketdata("b3-cotahist-daily")
+# brasa.process_marketdata("b3-lending-trades")
+# brasa.process_marketdata("b3-otc-trade-information")
+# brasa.process_marketdata("b3-economic-indicators-price")
+# brasa.process_marketdata("b3-economic-indicators-fwf")
+# brasa.process_marketdata("b3-equities-volatility-surface")
+# brasa.process_marketdata("b3-indexes-composition")
+# brasa.process_marketdata("b3-indexes-theoretical-portfolio")
+# brasa.process_marketdata("b3-listed-fixed-income-etfs")
+# brasa.process_marketdata("b3-listed-stock-etfs")
+# brasa.process_marketdata("b3-listed-reits")
+# brasa.process_marketdata("b3-trades-intraday")
